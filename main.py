2257314315546e2a554fd05e6c9d69309ad5d1e4
lulamae12 <tls.tommy11@gmail.com>
2024-12-11 14:32:14 -0500

    commit for now

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
 import os,subprocess
+from time import sleep
+
+import shutil
 
 try:
     import fichub_cli
@@ -23,7 +26,7 @@ class Downloader:
             print(f'Input directory, {INPUT_DIR}, doesnt exist!\nCreating!...\n')
             os.mkdir(INPUT_DIR)
             with open(os.path.join(INPUT_DIR,'infile.txt'), 'w') as file:
-                file.write('# Please enter the URL of each story as a new line.')
+                file.write('# Please enter the URL of each story as a new line.\n')
             
         if not os.path.exists(OUTPUT_DIR):
             print(f'Output directory, {OUTPUT_DIR}, doesnt exist!\nCreating!...\n')
@@ -61,36 +64,94 @@ class Downloader:
         return
     
     def menu_settings(self):
-        print('menu settings here')
+            os.system('cls||clear')
+            print('╔═════════════════════════════════════════════════╗')
+            print('║                   Settings Menu                 ║')
+            print('╟─────────────────────────────────────────────────╢')
+            print('║ [1] - Change Download format                    ║')
+            print('║ [2] - Rebuild Input/Output directories          ║')
+            print('║ [3] - Back to Main Menu                         ║')
+            print('╚═════════════════════════════════════════════════╝')
+            user_input = input('Please select an option (1-4). \nSelection: ').strip()
+            if(user_input == '1'):
+                
+
+                self.menu_single_download()
+            elif(user_input == '2'):
+                while True:
+                    print(f'\nYou entered: Rebuild Input/Output directories \n')
+                    print('WARNING: This is potentially destructive and will delete and recreate the input and output directories. \nALL existing data will be lost!')
+                    sleep(3)
+                    confirm = input('\nProceed? (Y/n):').strip().lower()
+                    if(confirm == 'y' or confirm == 'yes'):
+                        print('Rebuilding...')
+                        shutil.rmtree(INPUT_DIR, ignore_errors=True)
+                        os.mkdir(INPUT_DIR)
+                        with open(os.path.join(INPUT_DIR,'infile.txt'), 'w') as file:
+                            file.write('# Please enter the URL of each story as a new line.\n')
+                        print("Rebuilt input directory...")
+                        sleep(1)
+                        print(f'Output directory, {OUTPUT_DIR}, doesnt exist!\nCreating!...\n')
+                        shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+                        os.mkdir(OUTPUT_DIR)
+                        print("Rebuilt output directory...")
+                        sleep(1.5)
+                        return
+
+                    elif(confirm == 'n' or confirm == 'no'):
+                        print('Aborting!')
+                        return
+                    else:
+                        print("Error: unknown input!\nAborting!\n")
+                        sleep(1)
+                        return
+            elif(user_input == '3'):
+                return
+            
+            else: 
+                print('Error! Invalid choice!')
+
+    def basic_url_check(self,url):
+        if 'fanfiction.net' in url:
+            return True
+        else:
+            return False
 
     def menu_single_download(self):
         os.system('cls||clear')
         while True:
-            print('╔════════════════════════════')
-            print('║Download Single Story')
-            print(f'╟Format: {DOWNLOAD_FORMAT}')
-            print(f'╟Output Location: {OUTPUT_DIR}')
-            print("║Please enter the URl of the story below. \n║Type 'menu' to return to the main menu.")
-            print('║═══════════════════════════')
-            print('╚════════════════════════════')
-            url = input('Story URL: ').strip()
+            os.system('cls||clear')
+            print('╔═════════════════════════════════════════════════╗')
+            print('║              Download Single Story              ║')
+            print('╟─────────────────────────────────────────────────╢')
+            print(f'╟-Format:          {DOWNLOAD_FORMAT.ljust(31)}║')
+            print(f'╟-Output Location: {OUTPUT_DIR.ljust(31)}║')
+            print('╟─────────────────────────────────────────────────╢')
+            print("║ Type 'menu' to return to the main menu.         ║")
+            print('╚═════════════════════════════════════════════════╝')
+            url = input('Enter the URL to the story\nURL: ').strip()
             
             if(url.lower() == 'menu'):
                 return()
             
+            
             #confirm it
             while True:
+                
                 print(f'\nYou entered: {url.lower()}\n')
                 confirm = input('Proceed with download? (Y/n):').strip().lower()
                 if(confirm == 'y' or confirm == 'yes'):
                     print('Downloading...')
+                    
                     self.download(url.lower(),DOWNLOAD_FORMAT)
+                    return
 
                 elif(confirm == 'n' or confirm == 'no'):
                     print('Aborting!')
                     break
                 else:
                     print("Error: unknown input!\nPlease enter 'yes' or 'no'\n")
+                    sleep(1)
                     continue
 
 
@@ -106,43 +167,63 @@ class Downloader:
 
         os.system('cls||clear')
         while True:
-            print('╔════════════════════════════')
-            print('║Download multiple stories')
-            print(f'╟Format: {DOWNLOAD_FORMAT}')
-            print(f'╟Input File: {INPUT_DIR}/infile.txt')
-            print(f'╟Output Location: {OUTPUT_DIR}')
-            print(f"║Found {len(lines)} entries in 'infile.txt' \n║Type 'menu' to return to the main menu.")
+            print('╔═════════════════════════════════════════════════╗')
+            print('║             Download Muliple Stories            ║')
+            print('╟─────────────────────────────────────────────────╢')
             
-            print('╚════════════════════════════')
-            uinput = input("Press enter to contine. If you would like to use a diffrent file, type 'switch'.\n ").strip()
+            print(f'╟ Format: {DOWNLOAD_FORMAT.ljust(40)}║')
+            print(f'╟ Input File: {INPUT_DIR}/infile.txt'.ljust(50) + '║')
+            print(f'╟ Output Location: {OUTPUT_DIR.ljust(31)}║')
+            print('╟─────────────────────────────────────────────────╢')
             
-            if(uinput.lower() == 'menu'):
-                return()
-            
-            if(uinput.lower() == 'switch'):
-                return()
-            
-            #confirm it
-            while True:
+            print(f"║Found {len(lines)} entries in 'infile.txt'".ljust(50) + "║" + "\n║Type 'menu' to return to the main menu.          ║")
+            print('╚═════════════════════════════════════════════════╝')
+            uinput = input("Press Enter to start downloading or type 'switch' to use a different file. \nCommand: ").strip().lower()
+            if uinput == 'menu':
+                return
+            elif uinput == 'switch':
+                infile = input('\nPlease enter the full name of the file to you would like to read from.\nFilename: ')
+                while True:
+                    print('╔═════════════════════════════════════════════════╗')
+                    print('║             Download Muliple Stories            ║')
+                    print('╟─────────────────────────────────────────────────╢')
+                    
+                    print(f'╟ Format: {DOWNLOAD_FORMAT.ljust(40)}║')
+                    print(f'╟ Input File: {INPUT_DIR}/{infile}'.ljust(50) + '║')
+                    print(f'╟ Output Location: {OUTPUT_DIR.ljust(31)}║')
+                    print('╟─────────────────────────────────────────────────╢')
+                    
+                    print(f"║Found {len(lines)} entries in 'infile.txt'".ljust(50) + "║" + "\n║Type 'menu' to return to the main menu.          ║")
+                    print('╚═════════════════════════════════════════════════╝')
+                    uinput = input("Press Enter to start downloading.\nCommand: ").strip().lower()
+                    if uinput == 'menu':
+                        return
+                    else:
+                        self.download_from_file(os.path.join(INPUT_DIR,infile))
+                        return()
+            else:
+                self.download_from_file(os.path.join(INPUT_DIR,'infile.txt'))
 
     def menu(self):
         while True:
             
             os.system('cls||clear')
-            print('--------------------------------')
-            print('What would you like to do?')
-            print('[1] - Download a single story')
-            print('[2] - Download multiple stories from a file')
-            print('[3] - Settings')
-            print('[4] - Quit')
+            print('╔═════════════════════════════════════════════════╗')
+            print('║                    Main Menu                    ║')
+            print('╟─────────────────────────────────────────────────╢')
+            print('║ [1] - Download single story                     ║')
+            print('║ [2] - Download multiple stories from a file     ║')
+            print('║ [3] - Settings                                  ║')
+            print('║ [4] - Quit                                      ║')
+            print('╚═════════════════════════════════════════════════╝')
 
-            user_input = input('Please select an option (1-4): ').strip()
+            user_input = input('Please select an option (1-4). \nSelection: ').strip()
             if(user_input == '1'):
                 
 
                 self.menu_single_download()
             elif(user_input == '2'):
-                self.menu_bulk_download
+                self.menu_bulk_download()
             elif(user_input == '3'):
                 self.menu_settings()
             elif(user_input == '4'):
@@ -156,7 +237,7 @@ class Downloader:
 
 
 downloader = Downloader()
-#downloader.download('https://www.fanfiction.net/s/12806645/1/All-Dogs-Go-To-Valhalla','PDF')
+
 
 
 downloader.menu()
